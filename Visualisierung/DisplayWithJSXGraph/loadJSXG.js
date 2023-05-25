17f03c7c028d0fe5122c6e53bcf04bc58c39413a
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2023-05-25 09:45:39 +0200

    Bereich Visualisierung entfernt, da security alerts

--- Visualisierung/DisplayWithJSXGraph/loadJSXG.js
+++
@@ -1,8 +0,0 @@
-function loadJSXG(path)
-{
-	var selection = document.getElementById("selection");
-	fname = "http://localhost:8000/" + path + selection.options[selection.selectedIndex].value.toString();
-
-    $('debug').innerHTML = fname+'<br>';
-    board = JXG.JSXGraph.loadBoardFromFile('jxgbox', fname , 'Intergeo');
-}
